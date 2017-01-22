25f1da6260a92e42a11b3359c8fa4858f07bcebd
fvertiz <fva0986@me.com>
2017-01-22 14:18:32 -0600

    Update chat.js

--- Step2/chat.js
+++ Step2/chat.js
@@ -52,7 +52,39 @@
       'offline': offlineGreeting
       });
     });
-  } 
+  }
+  else if(locale === "pls") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+    onlineGreeting = "Porozmawiaj z nami";
+    offlineGreeting = 'Zostaw wiadomość';
+    $zopim(function() {
+      $zopim.livechat.setGreetings({
+      'online': onlineGreeting,
+      'offline': offlineGreeting
+      });
+    });
+  }
+else if(locale === "hndi") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+    onlineGreeting = "हमसे बात करो";
+    offlineGreeting = 'एक संदेश छोड़ दो';
+    $zopim(function() {
+      $zopim.livechat.setGreetings({
+      'online': onlineGreeting,
+      'offline': offlineGreeting
+      });
+    });
+  }
   else {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
     z._.push(c)},$=z.s=
